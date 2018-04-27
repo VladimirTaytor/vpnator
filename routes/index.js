532d4d27f697e6098be7703e598c359f6e417e0c
Vova Pihol <sg.volodymyr@gmail.com>
2018-04-28 00:57:57 +0300

    Add router for resources

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 /**
  *
@@ -9,16 +10,24 @@ const router = express.Router();
 module.exports = function (DAO, config) {
 
   /* GET home page. */
-  router.get('/', function(req, res, next) {
-    res.render('index', { title: 'VPNator' });
+  router.get('/', function (req, res, next) {
+    res.render('index', {title: 'VPNator'});
   });
 
-  router.get('/o/:token', function(req, res) {
+  router.get('/o/:token', function (req, res) {
     return DAO.getLinkByToken(req.params.token)
       .then(link => {
+        if(!link)
+          return res.end();
         const buffer = link.HTML.toString('utf8');
         res.send(buffer);
       });
+  });
+
+  router.get('/r', function (req, res) {
+    return request(req.query.l, function (error, response, body) {
+      res.send(body);
+    })
   });
 
   return router;
--- services/Processor.js
+++ services/Processor.js
@@ -41,7 +41,7 @@ class Processor {
   }
 
   static LINK_REGEXP() {
-    return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
+    return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/g;
   }
 }
 
